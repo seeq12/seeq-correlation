1ff0b5d9b39af124bc2bce1bbabcd963feb18327
ayobami <ayobami.ojekunle@seeq.com>
2024-09-06 20:22:58 +0100

    use status instead of quiet=True

--- seeq/addons/correlation/utils/_sdl.py
+++ seeq/addons/correlation/utils/_sdl.py
@@ -26,9 +26,9 @@ def pull_only_signals(url, grid='auto'):
     worksheet = spy.utils.get_analysis_worksheet_from_url(url)
     start = worksheet.display_range['Start']
     end = worksheet.display_range['End']
-    status=spy.Status(quiet=True)
+    status = spy.Status(quiet=True)
 
-    search_df = spy.search(url, estimate_sample_period=worksheet.display_range, quiet=True)
+    search_df = spy.search(url, estimate_sample_period=worksheet.display_range, status=status)
     if search_df.empty:
         return pd.DataFrame()
     search_signals_df = search_df[search_df['Type'].str.contains('Signal')]
