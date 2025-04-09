d4bd84909e7806fe988e5a0cfb86d0425597a934
AlexDorobantiu <AlexDorobantiu@users.noreply.github.com>
2025-04-09 16:11:07 +0300

    CRAB-48351: added compression to packaging

--- build.py
+++ build.py
@@ -32,7 +32,7 @@ distribution_relative_dir = 'dist'
 distribution_abs_dir = os.path.join(os.getcwd(), distribution_relative_dir)
 if os.path.isdir(distribution_abs_dir):
     shutil.rmtree(distribution_abs_dir)
-build_command = ['python3.8', 'setup.py', 'bdist_wheel',
+build_command = ['.venv/Scripts/python', 'setup.py', 'bdist_wheel',
                  '-d', distribution_relative_dir,
                  f'--python-tag=py{sys.version_info.major}{sys.version_info.minor}']
 subprocess.run(build_command, cwd=os.getcwd())
@@ -83,7 +83,7 @@ if args.addon:
     addon_meta = os.path.join(bin, f'{name}.addonmeta')
 
     # Build addon
-    with zipfile.ZipFile(addon, 'w') as z:
+    with zipfile.ZipFile(addon, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
         z.write(source_wheel, arcname=os.path.join('data-lab-functions', source_wheel_name))
         z.writestr('data-lab-functions/requirements.txt', f"./{source_wheel_name}")
         with z.open("addon.json", "w") as c:
@@ -100,7 +100,7 @@ if args.addon:
         addon_manager_artifacts.append(addon)
     # Build addonmeta
     print(f'Creating {name}.addonmeta')
-    with zipfile.ZipFile(addon_meta, 'w') as z:
+    with zipfile.ZipFile(addon_meta, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
         with z.open("addon.json", "w") as c:
             c.write(json.dumps(parsed_json, indent=2).encode("utf-8"))
         directory = pathlib.Path("./additional_content/")
