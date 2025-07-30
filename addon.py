5e49be07702afad699d3dcc6bcd767f31f5d170b
nicholasaraj <nicholas.araj@seeq.com>
2025-07-29 17:13:31 -0700

    Cleaned addon.py and updated dependencies

--- addon.py
+++ addon.py
@@ -4,27 +4,7 @@ import json
 import zipfile
 import pathlib
 import shutil
-import argparse
 import subprocess
-from pathlib import Path
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '-d', '--distribute',
-    help='Upload the built files to their distribution channels.',
-    action='store_true'
-)
-parser.add_argument(
-    '-c', '--compile',
-    help='Produce a compiled version of the code in addition to the source version',
-    action='store_true'
-)
-parser.add_argument(
-    '-a', '--addon',
-    help='Produce a zipped version for installation through the addon manager.',
-    action='store_true'
-)
-args = parser.parse_args()
 
 # build the distribution
 distribution_relative_dir = 'dist'
@@ -45,45 +25,44 @@ source_wheel_name = os.path.split(source_wheel)[-1]
 version = source_wheel_name.split('-')[1]
 
 addon_manager_artifacts = []
-if args.addon:
-    name = 'correlation'
-    print(f'Creating {name}.addon')
-    # Ensure output folder exists
-    bin = os.path.join(os.getcwd(), 'bin')
-    if not os.path.exists(bin):
-        os.makedirs(bin)
+name = 'correlation'
+print(f'Creating {name}.addon')
+# Ensure output folder exists
+bin = os.path.join(os.getcwd(), 'bin')
+if not os.path.exists(bin):
+    os.makedirs(bin)
 
-    with open('addon.json') as json_file:
-        parsed_json = json.load(json_file)
-    parsed_json['version'] = version
+with open('addon.json') as json_file:
+    parsed_json = json.load(json_file)
+parsed_json['version'] = version
 
-    addon = os.path.join(bin, f'{name}.addon')
-    addon_meta = os.path.join(bin, f'{name}.addonmeta')
+addon = os.path.join(bin, f'{name}.addon')
+addon_meta = os.path.join(bin, f'{name}.addonmeta')
 
-    # Build addon
-    with zipfile.ZipFile(addon, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
-        z.write(source_wheel, arcname=os.path.join('data-lab-functions', source_wheel_name))
-        z.writestr('data-lab-functions/requirements.txt', f"./{source_wheel_name}")
-        with z.open("addon.json", "w") as c:
-            c.write(json.dumps(parsed_json, indent=2).encode("utf-8"))
-        directory = pathlib.Path("./seeq/addons/correlation/deployment_notebook/")
-        for file in directory.rglob('*ipynb'):
-            z.write(file, arcname=os.path.join('data-lab-functions', file.name))
-        directory = pathlib.Path("./additional_content/")
-        for file in directory.iterdir():
-            z.write(file)
-        directory = pathlib.Path("./correlation_formulas/")
-        for file in directory.iterdir():
-            z.write(file)
-        addon_manager_artifacts.append(addon)
-    # Build addonmeta
-    print(f'Creating {name}.addonmeta')
-    with zipfile.ZipFile(addon_meta, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
-        with z.open("addon.json", "w") as c:
-            c.write(json.dumps(parsed_json, indent=2).encode("utf-8"))
-        directory = pathlib.Path("./additional_content/")
-        for file in directory.iterdir():
-            z.write(file)
-        addon_manager_artifacts.append(addon_meta)
+# Build addon
+with zipfile.ZipFile(addon, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
+    z.write(source_wheel, arcname=os.path.join('data-lab-functions', source_wheel_name))
+    z.writestr('data-lab-functions/requirements.txt', f"./{source_wheel_name}")
+    with z.open("addon.json", "w") as c:
+        c.write(json.dumps(parsed_json, indent=2).encode("utf-8"))
+    directory = pathlib.Path("./seeq/addons/correlation/deployment_notebook/")
+    for file in directory.rglob('*ipynb'):
+        z.write(file, arcname=os.path.join('data-lab-functions', file.name))
+    directory = pathlib.Path("./additional_content/")
+    for file in directory.iterdir():
+        z.write(file)
+    directory = pathlib.Path("./correlation_formulas/")
+    for file in directory.iterdir():
+        z.write(file)
+    addon_manager_artifacts.append(addon)
+# Build addonmeta
+print(f'Creating {name}.addonmeta')
+with zipfile.ZipFile(addon_meta, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as z:
+    with z.open("addon.json", "w") as c:
+        c.write(json.dumps(parsed_json, indent=2).encode("utf-8"))
+    directory = pathlib.Path("./additional_content/")
+    for file in directory.iterdir():
+        z.write(file)
+    addon_manager_artifacts.append(addon_meta)
 
-    print('Successfully created.')
+print('Successfully created.')
